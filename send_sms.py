677463582475cfd7c3582bcd5c4509d91b3eefb2
Christine Mitry <mitry.christine@gmail.com>
2019-12-16 20:15:03 -0500

    Update send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -1,13 +1,13 @@
 from twilio.rest import Client
 
-account_sid = "ACc7d820b58ebc6a23cf676a33eafa4887"
-auth_token = "495d0bf1748c77a9bdd9b939220e2711"
+account_sid = "########################################"
+auth_token = "######################################"
 client = Client(account_sid, auth_token)
 
 sms = client.messages.create(
-	body="ily let's go clubbing in sweatpants",
-    to="+14168585332",
-    from_="+18475581175",
+	body="hello!",
+    to="###",
+    from_="###",
 )
 
 print(sms.sid)
